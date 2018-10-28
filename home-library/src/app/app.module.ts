97e7fc1449449eb830bea3b71ed3b7202a97905e
Aleksandra Świderska <olaswiderska95@gmail.com>
2018-10-28 21:20:16 +0100

    Added server component to Angular part

--- home-library/src/app/app.module.ts
+++ home-library/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpClientModule} from "@angular/common/http";
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
