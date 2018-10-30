f6f3523ad6ed7ad595da06d5f9c6e4a9c64d4637
Aleksandra Świderska <olaswiderska95@gmail.com>
2018-10-30 22:34:49 +0100

    Moved informations from backend - from app component to server component

--- home-library/src/app/app.component.ts
+++ home-library/src/app/app.component.ts
@@ -1,7 +1,6 @@
 // src/app/app.component.ts
  
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
  
 @Component({
@@ -14,28 +13,5 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   name="projektTrello"
   title = 'PAW - projekt Trello ';
-  tables;
-  lists;
- 
-  constructor(private http: HttpClient) {
-   //http.get('http://localhost:3000/tables.json').subscribe(data => {
-   //  this.tables = data;
-     
-  //});
-   
-   for(var element in this.tables) {
-      
-    //http.get('http://localhost:3000/tables/'+element.id+'/lists.json').subscribe(data => element.list = data);
-  
-   }
-   
-   //http.get('https://jsonplaceholder.typicode.com/todos/').subscribe(data => this.books = data);
-  }
-
 }
 
-interface TableDomain {
-  name;
-  id;
-  list;
-}
--- home-library/src/app/server/server.component.ts
+++ home-library/src/app/server/server.component.ts
@@ -1,8 +1,31 @@
 import { Component } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html'
 })
 export class ServerComponent{
+    tables;
+    lists;
+   
+    constructor(private http: HttpClient) {
+     http.get('http://localhost:3000/tables.json').subscribe(data => {
+      this.tables = data;
+    });
+     
+     for(var element in this.tables) {
+        
+      //http.get('http://localhost:3000/tables/'+element.id+'/lists.json').subscribe(data => element.list = data);
+    
+     }
+     
+     //http.get('https://jsonplaceholder.typicode.com/todos/').subscribe(data => this.books = data);
+    }
 }
+
+interface TableDomain {
+    name;
+    id;
+    list;
+  }
