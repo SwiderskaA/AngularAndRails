04217862c9d8f45a1137a9294d170d2a4eb0fbaa
Aleksandra Świderska <olaswiderska95@gmail.com>
2018-11-04 22:43:55 +0100

    Created basics for logi in in the front end - edited login component

--- home-library/src/app/authorization/login/login.component.ts
+++ home-library/src/app/authorization/login/login.component.ts
@@ -1,11 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: 'app-authorization-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.css']
 })
-export class LoginComponent{
+export class LoginComponent implements OnInit{
+
+    constructor(){
+
+    }
+
+    ngOnInit(){
+
+    }
+
+    loginUser(event){
+        event.preventDefault()
+        const target = event.target
+        const username=target.querySelector('#email').value
+        const password=target.querySelector('#pwd').value
+        alert(username)
+        console.log(username,password)
+    }
 }
 
 
