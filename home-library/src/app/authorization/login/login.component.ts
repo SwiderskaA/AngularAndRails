3887af446c20abc700fae4fc565a3a7822229a48
Aleksandra Świderska <olaswiderska95@gmail.com>
2018-12-04 22:33:30 +0100

    Getting token for user from backend server response

--- home-library/src/app/authorization/login/login.component.ts
+++ home-library/src/app/authorization/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthService } from '../../auth.service';
+import { AngularWaitBarrier } from "blocking-proxy/built/lib/angular_wait_barrier";
+import { JsonPipe } from "@angular/common";
+
 
 @Component({
     selector: 'app-authorization-login',
@@ -7,6 +10,8 @@ import { AuthService } from '../../auth.service';
     styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit{
+    
+     token:String;
 
     constructor(private Auth:AuthService){
 
@@ -25,7 +30,8 @@ export class LoginComponent implements OnInit{
         console.log(username,password);
         this.Auth.getUserDetails(username,password).subscribe(data => {
             if(data){
-                alert("Login success !!! :) ");
+              this.token=data['authentication_token'];
+              alert("Your token is : " + this.token);
             }else{
                 alert(data);
             }
